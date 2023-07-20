e7f08efc3d31a5418b198d89b6c8850d77072b32
Hatem Lamia <117427433+HatemLamia@users.noreply.github.com>
2023-07-20 17:30:45 +0200

    Update ProductServices.js

--- src/service/ProductServices.js
+++ src/service/ProductServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/products";
+const API_URL = "http://localhost:4000/products";
 
 export const getProducts = () => {
   return axios.get(API_URL);
